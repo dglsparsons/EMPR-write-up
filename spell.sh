f19cde3a9d0c3c58ed464f0b81f37d8f4a5c7f90
Skeflas <dglsparsons@gmail.com>
2015-03-29 18:18:01 +0100

    Added a script for spellchecking, fancy headers, and made some more progress with chapter1

---
+++ spell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+aspell -t -c report.tex
+aspell -t -c chapter1.tex
+aspell -t -c chapter2.tex
+aspell -t -c chapter3.tex
+aspell -t -c chapter4.tex
+aspell -t -c chapter5.tex
+aspell -t -c chapter6.tex
+
+ispell -t report.tex
+ispell -t chapter1.tex
+ispell -t chapter2.tex
+ispell -t chapter3.tex
+ispell -t chapter4.tex
+ispell -t chapter5.tex
+ispell -t chapter6.tex
